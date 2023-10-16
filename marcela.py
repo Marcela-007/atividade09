70ae96b5b7c2fbc110c68db9cb7a3de619e84ec4
Marcela-007 <marcelaoliveira446677@gmail.com>
2023-10-16 15:25:16 -0300

    atividade09-menor ou maior

---
+++ marcela.py
@@ -0,0 +1,7 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+number1= int(input("Insira o primeiro valor: "))
+number2 = int(input("Insira o segundo valor: "))
+if number1 > number2:
+   print(" O número ", number1," é maior que ", number2," ")
+else:
+   print(" O número ", number1," é menor que ", number2," ")
